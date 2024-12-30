a44ff31b56a977733eee5a8ae81d24c9d87277b9
ミック(Sirapat) <sirapat.jr@gmail.com>
2024-12-30 11:46:41 +0700

    change from custom domain to github.io url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://sirapat.dev/", // replace this with your deployed domain
+  website: "https://extramix.github.io/", // replace this with your deployed domain
   author: "Sirapat Jariyanantakul",
   profile: "https://sirapat.dev/",
   desc: "Software Engineer",
